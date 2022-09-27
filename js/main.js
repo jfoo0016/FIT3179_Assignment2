013a3c7c333905eadfd49a7f4d8385ca5fefdf21
jfoo0016 <jfoo0016@student.monsah.edu>
2022-09-27 16:50:32 +0800

    update choropleth map title and add line chart (incomplete)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var spec2 = "js/choropleth_map.vl.json";
-vegaEmbed('#choropleth_map', spec2).then(function(result) {
+var spec = "js/choropleth_map.vl.json";
+vegaEmbed('#choropleth_map', spec).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec2 = "js/line_chart.vl.json";
+vegaEmbed('#line_chart', spec2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
