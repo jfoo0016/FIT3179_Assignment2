058e152fe23d4d50d7a34173ae92b00accdc6a6a
jfoo0016 <jfoo0016@student.monsah.edu>
2022-10-03 18:28:14 +0800

    added a placeholder version bar chart

--- js/main.js
+++ js/main.js
@@ -7,3 +7,8 @@ var spec2 = "js/line_chart.vl.json";
 vegaEmbed('#line_chart', spec2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var spec3 = "js/bar_chart.vl.json";
+vegaEmbed('#bar_chart', spec3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
