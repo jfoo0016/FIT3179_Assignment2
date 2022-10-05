3939382fc36afaaec9a032a9c33a6a486db84b86
jfoo0016 <jfoo0016@student.monsah.edu>
2022-10-06 00:58:56 +0800

    combined views

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-var spec = "js/choropleth_map.vl.json";
-vegaEmbed('#choropleth_map', spec).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+// var spec = "js/choropleth_map.vl.json";
+// vegaEmbed('#choropleth_map', spec).then(function(result) {
+// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
 
-var spec2 = "js/line_chart.vl.json";
-vegaEmbed('#line_chart', spec2).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+// var spec2 = "js/line_chart.vl.json";
+// vegaEmbed('#line_chart', spec2).then(function(result) {
+// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
 
-var spec3 = "js/bar_chart.vl.json";
-vegaEmbed('#bar_chart', spec3).then(function(result) {
+// var spec3 = "js/bar_chart.vl.json";
+// vegaEmbed('#bar_chart', spec3).then(function(result) {
+// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+var spec4 = "js/combined.vl.json";
+vegaEmbed('#combine', spec4).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
