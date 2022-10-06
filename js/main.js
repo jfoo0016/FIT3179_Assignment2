e8c33560b8c36090065d33444a872f92e060425f
jfoo0016 <jfoo0016@student.monsah.edu>
2022-10-06 18:11:09 +0800

    update dataset and other changes

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@
 // // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 // }).catch(console.error);
 
-// var spec3 = "js/bar_chart.vl.json";
-// vegaEmbed('#bar_chart', spec3).then(function(result) {
-// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
+var spec3 = "js/bar_chart.vl.json";
+vegaEmbed('#bar_chart', spec3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
 var spec4 = "js/combined.vl.json";
 vegaEmbed('#combine', spec4).then(function(result) {
