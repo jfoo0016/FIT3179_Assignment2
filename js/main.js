8c5bdadf5a74e1c804945062e283a9d19cf18bac
jfoo0016 <jfoo0016@student.monsah.edu>
2022-10-06 23:39:01 +0800

    some progress being made

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-// var spec = "js/choropleth_map.vl.json";
-// vegaEmbed('#choropleth_map', spec).then(function(result) {
-// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
-
-// var spec2 = "js/line_chart.vl.json";
-// vegaEmbed('#line_chart', spec2).then(function(result) {
-// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
-
-var spec3 = "js/bar_chart.vl.json";
-vegaEmbed('#bar_chart', spec3).then(function(result) {
+var spec = "js/choropleth_map.vl.json";
+vegaEmbed('#choropleth_map', spec, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var spec4 = "js/combined.vl.json";
-vegaEmbed('#combine', spec4).then(function(result) {
+var spec2 = "js/line_chart.vl.json";
+vegaEmbed('#line_chart', spec2, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec3 = "js/bar_chart.vl.json";
+vegaEmbed('#bar_chart', spec3, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec4 = "js/donut_chart.vl.json";
+vegaEmbed('#combine', spec4, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
