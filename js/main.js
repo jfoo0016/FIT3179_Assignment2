d62c2ad674f263aecb46a7bd9646881b06c06dbf
jfoo0016 <jfoo0016@student.monsah.edu>
2022-10-08 00:52:37 +0800

    continue working

--- js/main.js
+++ js/main.js
@@ -17,3 +17,9 @@ var spec4 = "js/donut_chart.vl.json";
 vegaEmbed('#donut_chart', spec4, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+
+var spec5 = "js/test.vl.json";
+vegaEmbed('#test', spec5, {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
