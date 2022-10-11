0d01ea23b67f011dc04294f083f28ae5c7aaa39d
jfoo0016 <jfoo0016@student.monsah.edu>
2022-10-11 21:59:16 +0800

    created treemap

--- js/main.js
+++ js/main.js
@@ -23,3 +23,13 @@ var spec5 = "js/scatter_plot.vl.json";
 vegaEmbed('#scatter_plot', spec5, {"actions": false}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+// const div = document.querySelector('.treemap')
+// fetch("C:/Users/FooJacky/Documents/2022 Sem 2/FIT3179/FIT3179_Assignment2/html/treemap.html")
+// .then(res => res.text())
+// .then(data => {
+//     div.innerHTML = data
+//     const parser = new DOMParser()
+//     const doc = parser.parseFromString(data, 'text/html')
+//     eval(doc.querySelector('script').textContent)
+// })
